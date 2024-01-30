69597ffb29d53f10719d81432adc2cdd8754d36d
sachin.p <sachin.p@corp.flock.com>
2024-01-30 11:21:34 +0530

    Updated Archive Mail Test

--- cypress/e2e/archive_email.cy.js
+++ cypress/e2e/archive_email.cy.js
@@ -7,29 +7,34 @@ describe('Archive Mail', () => {
     it('Archive', () => {
         cy.wait(3000)
 
-        // select mail to archive
+        // Select mail to archive and store it's Id
         cy.get('[class="list-rows pendo-Inbox-list"]').then( mails => {
             cy.wrap(mails).find('div').eq(0).as('selectedEmail')
             cy.get('@selectedEmail').invoke('attr', 'data-item-id').as('mailId')
             cy.get('@selectedEmail').click()
         })
         
+        // Click on archive button
         cy.get('[class="message-item-area"]').eq(0).click()
         cy.get('[data-testid="thread-archive-btn"]').click()
         cy.wait(3000)
 
+        // Go to archive section
         cy.get('[class="flockmail-outline-view"]').find('[class="category-content"]').eq(3).click()
-        
         cy.wait(3000)
 
+        // Find the Id of the Email archived
         cy.get('[data-item-id]', {timeout : 8000}).eq(0).invoke('attr', 'data-item-id').as('current')
 
+        // Match the Id's of both the mails
         cy.get('@current').then((currentValue) => {
             cy.get('@mailId').then((idValue) => {
               expect(currentValue).to.equal(idValue);
             });
-          });
+        });
+    })
 
+    after('Logout', () => {
         // logout
         cy.get('[data-testid="settings-icon-btn"]').click()
         cy.get('[data-testid="logout"]').click()
