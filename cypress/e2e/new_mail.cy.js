9b31b766abfdac7bca9ef4287617a61b381266df
aynp <aryan@titan.email>
2024-01-31 12:56:00 +0530

    Remove individual timeouts

--- cypress/e2e/new_mail.cy.js
+++ cypress/e2e/new_mail.cy.js
@@ -2,24 +2,20 @@
 
 context('Send Mail', () => {
     beforeEach(() => {
+        cy.intercept({ resourceType: /xhr | fetch/ }, { log: false })
         cy.login(Cypress.env('email'), Cypress.env('password'))
     })
 
     it('New mail', () => {
-        cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
-        cy.wait(3000);
-        cy.get('[data-testid="compose-btn"]', {
-            timeout: 20000
-        }).click();
+        cy.get('[data-testid="compose-btn"]').click();
+
         cy.get('.tokenizing-field-input').type(Cypress.env('email'))
         cy.get('.composer-subject > input').type("Hello World!")
         cy.get('.composer-editor').type("Hello World!")
 
         cy.get('.btn-send').click();
 
-        cy.get('.undo-message-wrapper', {
-            timeout: 10000
-        }).contains('Message sent!');
+        cy.get('.undo-message-wrapper').should('contain.text', 'Message sent!')
     })
 
     afterEach('Logout', () => {
