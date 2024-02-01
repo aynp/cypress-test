6393db7857a8bca0e48bca9a1e0f1c2a0d2e4743
sp <sp@BLQASACHINP.corp.flock.com>
2024-02-01 17:00:12 +0530

    Archive Email Test

--- cypress/e2e/pages/HomePage.js
+++ cypress/e2e/pages/HomePage.js
@@ -3,7 +3,13 @@ export class HomePage {
     pageElements = {
         settingButton : () => cy.get('[data-testid="settings-icon-btn"]'),
         darkModeButton : () => cy.get('[data-testid="enable_disable_darkmode"]'),
-        logedInEmail : () =>  cy.get('.email-address')    
+        logedInEmail : () =>  cy.get('.email-address'),
+        allMailsList : () => cy.get('[data-item-id]'),
+        threadArchiveButton : () => cy.get('.thread-toolbar-button-outer[data-idx="0"]'),
+        archiveSection : () => cy.get('#7'),
+        archiveToast : () => cy.get('.undo-message-wrapper'),
+        moreFolderButton : (config) => cy.get('.more-less-container is-collapsed add-account-sticky', config),
+        messageItemArea : () => cy.get('[class="message-item-area"]')  
     }
 
 }
