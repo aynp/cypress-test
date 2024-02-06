95403c5bdff514c7757b12254427f8a18f4abf51
aynp <aryan@titan.email>
2024-02-06 14:38:49 +0530

    Add comments for the code

--- cypress/e2e/bulkStar.cy.js
+++ cypress/e2e/bulkStar.cy.js
@@ -11,23 +11,30 @@ const selectedMailsThreadIds = [];
 
 context('Bulk Operations', () => {
     beforeEach(() => {
+        // disable logging for xhr and fetch requests
         cy.intercept({
             resourceType: /xhr | fetch/
         }, { log: false })
+
+        // login 
         cy.login(Cypress.env('email'), Cypress.env('password'))
     })
 
     it('Mark Star', () => {
+        // store thread ID of first 'selectCount' mails
         homePage.pageElements.selectFirstNMails(selectCount).each(($mail) => {
             cy.wrap($mail).invoke('attr', attrContainingMailId).then(($threadId) => {
+                // store thread ID in 'selectedMailsThreadIds'
                 selectedMailsThreadIds.push($threadId)
             })
         })
 
+        // select first 'selectCount' mails
         homePage.pageElements.mailSelectCheckbox(selectCount).each(elem => cy.wrap(elem).click());
 
-        // unstar all mails if any of them are starred already
+        // unstar all mails if all of them are starred already
         homePage.pageElements.starToggleButton().invoke('attr', 'class').then(($buttonClass) => {
+            // if the button is starred all the mails are already starred, unstar them all
             if ($buttonClass == starredClass) {
                 homePage.pageElements.starToggleButton().click()
             }
@@ -36,8 +43,10 @@ context('Bulk Operations', () => {
         // star all emails
         homePage.pageElements.starToggleButton().click();
 
+        // check toast message
         homePage.pageElements.toast().should('contain.text', `${selectCount} conversations starred`)
 
+        // intercept API request and check if all the selected mails are bening sent
         cy.intercept('POST', starApiUrl).as('starApiUrl')
         cy.wait('@starApiUrl').then((interception) => {
             const threadsSentList = JSON.parse(interception.request.body.reqs[0].p).ttxn
@@ -49,7 +58,8 @@ context('Bulk Operations', () => {
         })
     })
 
-    afterEach('Logout', () => {
+    afterEach(() => {
+        // logout
         cy.logout(Cypress.env('email'), Cypress.env('password'))
     })
 })
